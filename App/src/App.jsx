f82ffcc309d329df06712333b3c3630dc4baf01d
ABDUR-RAHIM1 <abdurrahim88557@gmail.com>
2024-09-02 23:07:01 +0600

    ready template

---
+++ App/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className=' w-full h-screen flex items-center justify-center text-2xl'>React Ready Template</div>
+  )
+}
